95fa35015311572f0082ce67799b1250eb001547
TheGlitch76 <glitchieproductionsofficial@gmail.com>
2019-12-30 18:24:16 -0600

    IJ format LogicalSidedProvider

--- patchwork-fml/src/main/java/net/minecraftforge/fml/LogicalSidedProvider.java
+++ patchwork-fml/src/main/java/net/minecraftforge/fml/LogicalSidedProvider.java
@@ -12,42 +12,38 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import com.patchworkmc.impl.fml.PatchworkFML;
 
-public enum LogicalSidedProvider
-{
-	WORKQUEUE((c)->c.get(), (s)->s.get()),
-	INSTANCE((c)->c.get(), (s)->s.get()),
-	CLIENTWORLD((c)-> Optional.<World>of(c.get().world), (s)-> Optional.<World>empty())
-	;
+public enum LogicalSidedProvider {
+	WORKQUEUE((c) -> c.get(), (s) -> s.get()),
+	INSTANCE((c) -> c.get(), (s) -> s.get()),
+	CLIENTWORLD((c) -> Optional.<World>of(c.get().world), (s) -> Optional.<World>empty());
 	private static Supplier<MinecraftClient> client;
 	private static Supplier<MinecraftServer> server;
 
-	LogicalSidedProvider(Function<Supplier<MinecraftClient>, ?> clientSide, Function<Supplier<MinecraftServer>, ?> serverSide)
-	{
-		this.clientSide = clientSide;
-		this.serverSide = serverSide;
-	}
 	// Patchwork: since the client never changes we can just set it directly
 	static {
-		if(FabricLoader.getInstance().getEnvironmentType().equals(EnvType.CLIENT)) {
+		if (FabricLoader.getInstance().getEnvironmentType().equals(EnvType.CLIENT)) {
 			client = () -> (MinecraftClient) FabricLoader.getInstance().getGameInstance();
 		}
+	}
+
+	private final Function<Supplier<MinecraftClient>, ?> clientSide;
+	private final Function<Supplier<MinecraftServer>, ?> serverSide;
+
+	LogicalSidedProvider(Function<Supplier<MinecraftClient>, ?> clientSide, Function<Supplier<MinecraftServer>, ?> serverSide) {
+		this.clientSide = clientSide;
+		this.serverSide = serverSide;
 	}
 
 	/**
 	 * Called by callbacks registered in {@link PatchworkFML}
 	 */
-	public static void setServer(Supplier<MinecraftServer> server)
-	{
+	public static void setServer(Supplier<MinecraftServer> server) {
 
 		LogicalSidedProvider.server = server;
 	}
 
-
-	private final Function<Supplier<MinecraftClient>, ?> clientSide;
-	private final Function<Supplier<MinecraftServer>, ?> serverSide;
-
 	@SuppressWarnings("unchecked")
 	public <T> T get(final LogicalSide side) {
-		return (T)(side==LogicalSide.CLIENT ? clientSide.apply(client) : serverSide.apply(server));
+		return (T) (side == LogicalSide.CLIENT ? clientSide.apply(client) : serverSide.apply(server));
 	}
 }
