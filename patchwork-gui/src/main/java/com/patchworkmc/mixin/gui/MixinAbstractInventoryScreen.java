0b230eb2f4b63cbbcda2c6bc2be34f5b36b7dd2b
shedaniel <daniel@shedaniel.me>
2020-01-18 16:30:49 +0800

    change modules

--- patchwork-gui/src/main/java/com/patchworkmc/mixin/gui/MixinAbstractInventoryScreen.java
+++ patchwork-gui/src/main/java/com/patchworkmc/mixin/gui/MixinAbstractInventoryScreen.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package com.patchworkmc.mixin.rendering;
+package com.patchworkmc.mixin.gui;
 
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
--- patchwork-gui/src/main/java/com/patchworkmc/mixin/gui/MixinScreen.java
+++ patchwork-gui/src/main/java/com/patchworkmc/mixin/gui/MixinScreen.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package com.patchworkmc.mixin.rendering;
+package com.patchworkmc.mixin.gui;
 
 import java.util.List;
 import java.util.function.Consumer;
