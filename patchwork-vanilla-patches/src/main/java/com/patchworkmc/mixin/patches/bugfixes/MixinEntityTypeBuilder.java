d4f43d3eb103633673aec250f0e2409b7bd1210d
TheGlitch76 <glitchieproductionsofficial@gmail.com>
2020-01-19 15:50:14 -0600

    Fix checkstyle + add license headers

--- patchwork-vanilla-patches/src/main/java/com/patchworkmc/mixin/patches/bugfixes/MixinEntityTypeBuilder.java
+++ patchwork-vanilla-patches/src/main/java/com/patchworkmc/mixin/patches/bugfixes/MixinEntityTypeBuilder.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge, Patchwork Project
+ * Copyright (c) 2016-2019, 2019
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package com.patchworkmc.mixin.patches.bugfixes;
 
 import org.apache.logging.log4j.LogManager;
@@ -61,5 +80,4 @@ public abstract class MixinEntityTypeBuilder<T extends Entity> {
 			return new EntityType(this.factory, this.category, this.saveable, this.summonable, this.fireImmune, this.field_19424, this.size);
 		}
 	}
-
 }
