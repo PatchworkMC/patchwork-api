7c07a984fd2d983a56fbdb11a845c87a5a9bd637
valoeghese <valoeghesemods@gmail.com>
2020-01-25 17:09:53 +1300

    import order 2

--- patchwork-biomes/src/main/java/com/patchworkmc/mixin/biomes/MixinBiome.java
+++ patchwork-biomes/src/main/java/com/patchworkmc/mixin/biomes/MixinBiome.java
@@ -1,10 +1,11 @@
 package com.patchworkmc.mixin.biomes;
 
-import com.patchworkmc.impl.biomes.ForgeBiomeExt;
-import com.patchworkmc.impl.biomes.PatchworkBiomes;
 import org.spongepowered.asm.mixin.Mixin;
 
 import net.minecraft.world.biome.Biome;
+
+import com.patchworkmc.impl.biomes.ForgeBiomeExt;
+import com.patchworkmc.impl.biomes.PatchworkBiomes;
 
 @Mixin(Biome.class)
 public class MixinBiome implements ForgeBiomeExt {
