e06e10284309a922d5e836f3767908182259b865
Ramid Khan <ramidzkh@gmail.com>
2020-02-29 11:08:03 +1100

    Add methods to vanilla classes which have capabilities

--- patchwork-capabilities/src/main/java/com/patchworkmc/impl/capability/CapabilityProviderHolder.java
+++ patchwork-capabilities/src/main/java/com/patchworkmc/impl/capability/CapabilityProviderHolder.java
@@ -20,13 +20,68 @@
 package com.patchworkmc.impl.capability;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityDispatcher;
 import net.minecraftforge.common.capabilities.CapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.math.Direction;
 
 /**
  * A holder for {@link CapabilityProvider}, since some classes cannot directly extend {@link CapabilityProvider}.
  */
-public interface CapabilityProviderHolder {
+public interface CapabilityProviderHolder extends ICapabilityProvider {
 	@Nonnull
 	CapabilityProvider<?> getCapabilityProvider();
+
+	default void gatherCapabilities() {
+		getCapabilityProvider().gatherCapabilities();
+	}
+
+	default void gatherCapabilities(@Nullable ICapabilityProvider parent) {
+		getCapabilityProvider().gatherCapabilities(parent);
+	}
+
+	default CapabilityDispatcher getCapabilities() {
+		return getCapabilityProvider().getCapabilities();
+	}
+
+	default boolean areCapsCompatible(CapabilityProvider<?> other) {
+		return getCapabilityProvider().areCapsCompatible((CapabilityProvider) other);
+	}
+
+	default boolean areCapsCompatible(CapabilityDispatcher other) {
+		return getCapabilityProvider().areCapsCompatible(other);
+	}
+
+	@Nullable
+	default CompoundTag serializeCaps() {
+		return getCapabilityProvider().serializeCaps();
+	}
+
+	default void deserializeCaps(CompoundTag tag) {
+		getCapabilityProvider().deserializeCaps(tag);
+	}
+
+	default void invalidateCaps() {
+		getCapabilityProvider().invalidateCaps();
+	}
+
+	default void reviveCaps() {
+		getCapabilityProvider().reviveCaps();
+	}
+
+	@Nonnull
+	default <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+		return getCapabilityProvider().getCapability(cap, side);
+	}
+
+	@Nonnull
+	default <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap) {
+		return getCapabilityProvider().getCapability(cap);
+	}
 }
