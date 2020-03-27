e231c78f80f2e7bd0944d99362f73d2cc71f0eff
qouteall <qouteall@outlook.com>
2020-03-27 17:23:32 +0800

    fix license

--- patchwork-extensions/src/main/java/net/patchworkmc/mixin/extension/MixinRecipeGridAligner.java
+++ patchwork-extensions/src/main/java/net/patchworkmc/mixin/extension/MixinRecipeGridAligner.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge, Patchwork Project
+ * Copyright (c) 2016-2020, 2019-2020
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.patchworkmc.mixin.extension;
 
 import java.util.Iterator;
@@ -11,13 +30,13 @@ import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeGridAligner;
 
 @Mixin(RecipeGridAligner.class)
-public interface MixinRecipeGridAligner {
+public class MixinRecipeGridAligner {
 	@ModifyVariable(
 			method = "alignRecipeToGrid",
 			at = @At("HEAD"),
 			ordinal = 0
 	)
-	default int modifyGridWidth(int gridWidth, int gridHeight, int gridOutputSlot, Recipe<?> recipe, Iterator<?> inputs, int amount) {
+	private int modifyGridWidth(int gridWidth, int gridHeight, int gridOutputSlot, Recipe<?> recipe, Iterator<?> inputs, int amount) {
 		if (recipe instanceof IShapedRecipe) {
 			return ((IShapedRecipe<?>) recipe).getRecipeWidth();
 		}
@@ -30,7 +49,7 @@ public interface MixinRecipeGridAligner {
 			at = @At("HEAD"),
 			ordinal = 1
 	)
-	default int modifyGridHeight(int gridWidth, int gridHeight, int gridOutputSlot, Recipe<?> recipe, Iterator<?> inputs, int amount) {
+	private int modifyGridHeight(int gridWidth, int gridHeight, int gridOutputSlot, Recipe<?> recipe, Iterator<?> inputs, int amount) {
 		if (recipe instanceof IShapedRecipe) {
 			return ((IShapedRecipe<?>) recipe).getRecipeHeight();
 		}
