b6218d022f0e9923311936d4600d36aa50afdb1d
qouteall <qouteall@outlook.com>
2020-05-19 05:54:20 +0800

    Implement Annotation Scanning (#65)
    * Revert "Implement IShapedRecipe and add new module patchwork-recipes"
    
    This reverts commit b020b67e7391164a458976ae0ea00ff44b8c9f14.
    
    * Revert "Implement IShapedRecipe and add new module patchwork-recipes"
    
    This reverts commit c59ede6a0a743c8934341a28e35272fb14b37640.
    
    * change the way of getting parent mod container
    
    * add getAllScanData
    
    * small refactor
    
    * make sub mods share the same ModFileInfo
    
    * change to patchwork:parent
    
    * remove unused method
    
    * fix annotation scanning detail
    
    * fix annotation scanning detail
    
    * minor fix
    
    * Revert "Revert "Annotation Scanning""
    
    This reverts commit b36fc8bac5a127c8b67c44f5b343441556a67391.
    
    * fix broken branch

--- patchwork-fml/src/main/java/net/minecraftforge/fml/ModList.java
+++ patchwork-fml/src/main/java/net/minecraftforge/fml/ModList.java
@@ -19,11 +19,29 @@
 
 package net.minecraftforge.fml;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import net.minecraftforge.fml.loading.moddiscovery.ModFileInfo;
+import net.minecraftforge.forgespi.language.ModFileScanData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.CustomValue;
 
 public class ModList {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	// Patchwork: initalize directly because there's no args
 	private static ModList INSTANCE = new ModList();
+
+	private Map<String, ModFileInfo> modFileInfoMap = new HashMap<>();
+
+	private List<ModFileScanData> allScanDataCache;
 
 	public static ModList get() {
 		return INSTANCE;
@@ -32,5 +50,76 @@ public class ModList {
 	public boolean isLoaded(String modId) {
 		// Patchwork: use Fabric Loader lookup instead of an internal one
 		return FabricLoader.getInstance().isModLoaded(modId);
+	}
+
+	public ModFileInfo getModFileById(String modId) {
+		return modFileInfoMap.computeIfAbsent(
+				modId,
+				k -> {
+					String annotationHolderModid = getAnnotationHolderModid(modId);
+
+					if (annotationHolderModid == null) {
+						//non-patched mod or invalid mod
+						return new ModFileInfo();
+					}
+
+					if (annotationHolderModid.equals(modId)) {
+						//Patched mod and is not jij
+						return new ModFileInfo(annotationHolderModid);
+					}
+
+					//jij patched mod
+					return getModFileById(annotationHolderModid);
+				}
+		);
+	}
+
+	public List<ModFileScanData> getAllScanData() {
+		if (allScanDataCache == null) {
+			allScanDataCache = FabricLoader.getInstance().getAllMods()
+					.stream()
+					.map(modContainer -> modContainer.getMetadata().getId())
+					.map(modid -> getModFileById(modid).getFile().getScanResult())
+					.distinct()
+					.collect(Collectors.toList());
+		}
+
+		return allScanDataCache;
+	}
+
+	//return null if it does not have annotation data
+	private static String getAnnotationHolderModid(String modid) {
+		ModContainer modContainer = FabricLoader.getInstance().getModContainer(modid).orElse(null);
+
+		if (modContainer == null) {
+			LOGGER.error("Trying to access annotation data of a missing mod " + modid);
+			LOGGER.catching(new Throwable());
+			return null;
+		}
+
+		if (!isPatchedMod(modContainer)) {
+			return null;
+		}
+
+		CustomValue parent = modContainer.getMetadata().getCustomValue("patchwork:parent");
+
+		if (parent == null) {
+			//it's not a jij mod
+			return modid;
+		}
+
+		return parent.getAsString();
+	}
+
+	public static boolean isPatchedMod(ModContainer modContainer) {
+		CustomValue source = modContainer.getMetadata().getCustomValue("patchwork:source");
+
+		if (source == null) {
+			return false;
+		}
+
+		CustomValue.CvObject object = source.getAsObject();
+		String loader = object.get("loader").getAsString();
+		return loader.equals("forge");
 	}
 }
---
+++ patchwork-fml/src/main/java/net/minecraftforge/forgespi/language/ModFileScanData.java
@@ -0,0 +1,250 @@
+/*
+ * Minecraft Forge, Patchwork Project
+ * Copyright (c) 2016-2020, 2019-2020
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.forgespi.language;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.Type;
+
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.metadata.CustomValue;
+
+public class ModFileScanData {
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public static final ModFileScanData EMPTY = new ModFileScanData();
+
+	ModContainer modContainer;
+	private boolean initialized = false;
+	private AnnotationStorage annotationStorage;
+	private Set<AnnotationData> annotationData;
+
+	public ModFileScanData(ModContainer modContainer) {
+		this.modContainer = modContainer;
+	}
+
+	//create empty mod file scan data for Fabric mods
+	private ModFileScanData() {
+		initialized = true;
+		annotationData = Collections.emptySet();
+	}
+
+	private void init() {
+		initialized = true;
+
+		CustomValue customValue = modContainer.getMetadata().getCustomValue("patchwork:annotations");
+
+		if (customValue == null) {
+			LOGGER.error("ModFileScanData: Tried to access the scanned annotation data for " + getModid() + ", but it is missing");
+			annotationData = Collections.emptySet();
+			return;
+		}
+
+		String annotationJsonLocation = customValue.getAsString();
+		Path annotationJsonPath = modContainer.getPath(annotationJsonLocation);
+
+		try {
+			InputStream outputStream = Files.newInputStream(annotationJsonPath);
+			Gson gson = new Gson();
+			this.annotationStorage = gson.fromJson(
+					new InputStreamReader(outputStream), AnnotationStorage.class
+			);
+			annotationData = this.annotationStorage.entries.stream()
+					.map(ModFileScanData::getAnnotationData)
+					.collect(Collectors.toSet());
+		} catch (IOException e) {
+			LOGGER.error(String.format(
+					"Could not read annotations from %s %s (loaded from %s)",
+					getModid(), annotationJsonPath, modContainer.getRootPath()
+			));
+			e.printStackTrace();
+			annotationData = Collections.emptySet();
+		}
+	}
+
+	public Set<AnnotationData> getAnnotations() {
+		if (!initialized) {
+			init();
+		}
+
+		return annotationData;
+	}
+
+	private static AnnotationData getAnnotationData(AnnotationStorage.Entry entry) {
+		Type annotationType = Type.getType(entry.annotationType);
+		Type targetInType = Type.getType("L" + entry.targetInClass + ";");
+		return new AnnotationData(
+				annotationType, entry.targetType, targetInType, entry.target
+		);
+	}
+
+	public String getModid() {
+		return modContainer.getMetadata().getId();
+	}
+
+	public static class AnnotationData {
+		private final Type annotationType;
+		private final ElementType targetType;
+		private final Type clazz;
+		private final String memberName;
+
+		//lazy evaluated
+		private Map<String, Object> annotationData;
+
+		public AnnotationData(
+				final Type annotationType, final ElementType targetType,
+				final Type clazz, final String memberName
+		) {
+			this.annotationType = annotationType;
+			this.targetType = targetType;
+			this.clazz = clazz;
+			this.memberName = memberName;
+		}
+
+		public Type getAnnotationType() {
+			return annotationType;
+		}
+
+		public ElementType getTargetType() {
+			return targetType;
+		}
+
+		public Type getClassType() {
+			return clazz;
+		}
+
+		public String getMemberName() {
+			return memberName;
+		}
+
+		public Map<String, Object> getAnnotationData() {
+			if (annotationData == null) {
+				initAnnotationData();
+			}
+
+			return annotationData;
+		}
+
+		private void initAnnotationData() {
+			annotationData = new HashMap<>();
+
+			try {
+				// TODO: This *may* load classes in the wrong order, but it shouldn't be an issue
+				Class<?> clazzObj = Class.forName(clazz.getClassName());
+				Class annotationType = Class.forName(this.annotationType.getClassName());
+				Annotation annotationObject = getAnnotationObject(clazzObj, annotationType);
+
+				if (annotationObject == null) {
+					LOGGER.error(String.format("Cannot fetch annotation object %s %s %s %s",
+							annotationType, targetType, clazz, memberName
+					));
+					return;
+				}
+
+				Method[] argMethods = annotationObject.getClass().getDeclaredMethods();
+
+				for (Method argMethod : argMethods) {
+					if (isArgumentMethod(argMethod)) {
+						annotationData.put(
+								argMethod.getName(),
+								argMethod.invoke(annotationObject)
+						);
+					}
+				}
+			} catch (Throwable e) {
+				LOGGER.catching(e);
+			}
+		}
+
+		private static boolean isArgumentMethod(Method method) {
+			String name = method.getName();
+			if (name.equals("toString")) return false;
+			if (name.equals("hashCode")) return false;
+			if (name.equals("getClass")) return false;
+			if (name.equals("equals")) return false;
+			if (name.equals("annotationType")) return false;
+
+			return true;
+		}
+
+		private Annotation getAnnotationObject(Class<?> clazzObj, Class annotationType) throws Throwable {
+			switch (targetType) {
+			case TYPE:
+				return clazzObj.getAnnotation(annotationType);
+			case FIELD:
+				return clazzObj.getField(memberName)
+						.getAnnotation(annotationType);
+			case METHOD:
+				String methodName = memberName.substring(0, memberName.indexOf('('));
+				Method[] methods = Arrays.stream(clazzObj.getDeclaredMethods())
+					.filter(method -> method.getName().equals(methodName))
+					.toArray(Method[]::new);
+				if (methods.length == 0) {
+					throw new RuntimeException("Cannot find method " + methodName);
+				}
+
+				if (methods.length > 1) {
+					//TODO handle overloaded methods
+
+					throw new RuntimeException("Currently Cannot Handle Overloaded Methods");
+				}
+
+				return methods[0].getAnnotation(annotationType);
+			default:
+				throw new RuntimeException("Invalid annotation type " + targetType);
+			}
+		}
+
+		@Override
+		public boolean equals(final Object obj) {
+			if (!(obj instanceof AnnotationData)) {
+				return false;
+			}
+
+			AnnotationData dat = (AnnotationData) obj;
+			return Objects.equals(annotationType, dat.annotationType)
+					&& Objects.equals(targetType, dat.targetType)
+					&& Objects.equals(clazz, dat.clazz)
+					&& Objects.equals(memberName, dat.memberName);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(annotationType, targetType, clazz, memberName);
+		}
+	}
+}
