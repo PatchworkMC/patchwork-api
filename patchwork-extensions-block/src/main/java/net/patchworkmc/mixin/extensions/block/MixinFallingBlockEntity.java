685d99e96c8c4060d4554da38eb6e32a0059a4c7
Rikka0w0 <929513338@qq.com>
2020-06-23 07:07:57 +1000

    Alternative impl of MixinFallingBlockEntity, since Mixin cannot capture correct local variables in FallingBlockEntity.tick()

--- patchwork-extensions-block/src/main/java/net/patchworkmc/mixin/extensions/block/MixinFallingBlockEntity.java
+++ patchwork-extensions-block/src/main/java/net/patchworkmc/mixin/extensions/block/MixinFallingBlockEntity.java
@@ -20,40 +20,24 @@
 package net.patchworkmc.mixin.extensions.block;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.FallingBlockEntity;
-import net.minecraft.util.math.BlockPos;
 
 import net.patchworkmc.impl.extensions.block.BlockContext;
-import net.patchworkmc.impl.extensions.block.Signatures;
 
 @Mixin(FallingBlockEntity.class)
 public abstract class MixinFallingBlockEntity {
 	////////////////////////
 	/// tick()
 	////////////////////////
-	private static final ThreadLocal<Object> tick_blockState = BlockContext.createContext();
 	// } else if (block2 != block && block2 instanceof BlockEntityProvider) {
-	@Inject(method = "tick", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "CONSTANT", args = Signatures.PATCHWORK_YARN_CLS_BLOCKENTITYPROVIDER, ordinal = 0, shift = Shift.BEFORE))
-	public void patchwork_yarn_tick_instanceof_BlockEntityProvider(CallbackInfo ci, Block block, BlockPos blockPos2) {
-		FallingBlockEntity me = (FallingBlockEntity) (Object) this;
-		BlockState blockState = me.world.getBlockState(blockPos2);
-		BlockContext.pushContext(tick_blockState, blockState);
-	}
-
 	@ModifyConstant(method = "tick", constant = @Constant(classValue = BlockEntityProvider.class, ordinal = 0))
 	public boolean patchwork_tick_instanceof_BlockEntityProvider(Object object, Class<?> clazz) {
-		BlockState blockState2 = BlockContext.popContext(tick_blockState);
-		return BlockContext.hasBlockEntity(blockState2);
+		FallingBlockEntity me = (FallingBlockEntity) (Object) this;
+		// Forge: return ((Block) object).hasBlockEntity();
+		return BlockContext.hasBlockEntity(me.getBlockState());
 	}
 }
