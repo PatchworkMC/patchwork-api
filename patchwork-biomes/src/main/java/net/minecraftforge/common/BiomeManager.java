30bd03374a0a43f6b659c224f3ecec72e31ff0ca
leo60228 <leo@60228.dev>
2020-07-08 11:48:42 -0400

    Switch from ArrayList to TrackedList in BiomeManager

--- patchwork-biomes/src/main/java/net/minecraftforge/common/BiomeManager.java
+++ patchwork-biomes/src/main/java/net/minecraftforge/common/BiomeManager.java
@@ -19,6 +19,7 @@
 
 package net.minecraftforge.common;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
@@ -84,11 +85,11 @@ public class BiomeManager {
 	}
 
 	// Biomes O' Plenty pokes Forge internals. Fun
-	private static ArrayList<BiomeEntry>[] biomes = setupBiomes();
+	private static TrackedList<BiomeEntry>[] biomes = setupBiomes();
 
-	private static ArrayList<BiomeEntry>[] setupBiomes() {
+	private static TrackedList<BiomeEntry>[] setupBiomes() {
 		@SuppressWarnings("unchecked")
-		ArrayList<BiomeEntry>[] currentBiomes = new ArrayList[BiomeType.values().length];
+		TrackedList<BiomeEntry>[] currentBiomes = new TrackedList[BiomeType.values().length];
 		List<BiomeEntry> list = new ArrayList<BiomeEntry>();
 
 		list.add(new BiomeEntry(Biomes.FOREST, 10));
@@ -98,7 +99,7 @@ public class BiomeManager {
 		list.add(new BiomeEntry(Biomes.BIRCH_FOREST, 10));
 		list.add(new BiomeEntry(Biomes.SWAMP, 10));
 
-		currentBiomes[BiomeType.WARM.ordinal()] = new ArrayList<BiomeEntry>(list);
+		currentBiomes[BiomeType.WARM.ordinal()] = new TrackedList<BiomeEntry>(list);
 		list.clear();
 
 		list.add(new BiomeEntry(Biomes.FOREST, 10));
@@ -106,16 +107,16 @@ public class BiomeManager {
 		list.add(new BiomeEntry(Biomes.TAIGA, 10));
 		list.add(new BiomeEntry(Biomes.PLAINS, 10));
 
-		currentBiomes[BiomeType.COOL.ordinal()] = new ArrayList<BiomeEntry>(list);
+		currentBiomes[BiomeType.COOL.ordinal()] = new TrackedList<BiomeEntry>(list);
 		list.clear();
 
 		list.add(new BiomeEntry(Biomes.SNOWY_TUNDRA, 30));
 		list.add(new BiomeEntry(Biomes.SNOWY_TAIGA, 10));
 
-		currentBiomes[BiomeType.ICY.ordinal()] = new ArrayList<BiomeEntry>(list);
+		currentBiomes[BiomeType.ICY.ordinal()] = new TrackedList<BiomeEntry>(list);
 		list.clear();
 
-		currentBiomes[BiomeType.DESERT.ordinal()] = new ArrayList<BiomeEntry>(list);
+		currentBiomes[BiomeType.DESERT.ordinal()] = new TrackedList<BiomeEntry>(list);
 
 		return currentBiomes;
 	}
@@ -125,5 +126,78 @@ public class BiomeManager {
 		List<BiomeEntry> list = idx >= biomes.length ? null : biomes[idx];
 
 		return list != null ? ImmutableList.copyOf(list) : null;
+	}
+
+	private static class TrackedList<E> extends ArrayList<E> {
+		private static final long serialVersionUID = 1L;
+		private boolean isModded = false;
+
+		TrackedList(Collection<? extends E> c) {
+			super(c);
+		}
+
+		@Override
+		public E set(int index, E element) {
+			isModded = true;
+			return super.set(index, element);
+		}
+
+		@Override
+		public boolean add(E e) {
+			isModded = true;
+			return super.add(e);
+		}
+
+		@Override
+		public void add(int index, E element) {
+			isModded = true;
+			super.add(index, element);
+		}
+
+		@Override
+		public E remove(int index) {
+			isModded = true;
+			return super.remove(index);
+		}
+
+		@Override
+		public boolean remove(Object o) {
+			isModded = true;
+			return super.remove(o);
+		}
+
+		@Override
+		public void clear() {
+			isModded = true;
+			super.clear();
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends E> c) {
+			isModded = true;
+			return super.addAll(c);
+		}
+
+		@Override
+		public boolean addAll(int index, Collection<? extends E> c) {
+			isModded = true;
+			return super.addAll(index, c);
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			isModded = true;
+			return super.removeAll(c);
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			isModded = true;
+			return super.retainAll(c);
+		}
+
+		public boolean isModded() {
+			return isModded;
+		}
 	}
 }
