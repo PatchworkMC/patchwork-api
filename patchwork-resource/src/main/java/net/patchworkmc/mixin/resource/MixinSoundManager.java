1bafda1a110fc8dbe65ea330e58282b1362f28cb
Rikka0_0小六花 <929513338@qq.com>
2020-07-12 05:16:50 +1000

    Impl Forge's selective resource reloader (#107)
    * Implement ISelectiveResourceReloadListener
    
    * Add other ResourceTypes, add readme, remove patchwork-fml dependency
    
    * Apply suggestions from code review
    
    Co-authored-by: Glitch <glitchieproductionsofficial@gmail.com>
    
    * Made lambda injection working by disabling remap
    
    * Fix json indentation
    
    * Update patchwork-resource/src/main/java/net/patchworkmc/mixin/resource/MixinLanguageOptionsScreen.java
    
    Co-authored-by: Glitch <glitchieproductionsofficial@gmail.com>
    
    Co-authored-by: Glitch <glitchieproductionsofficial@gmail.com>

---
+++ patchwork-resource/src/main/java/net/patchworkmc/mixin/resource/MixinSoundManager.java
@@ -0,0 +1,36 @@
+/*
+ * Minecraft Forge, Patchwork Project
+ * Copyright (c) 2016-2020, 2019-2020
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.patchworkmc.mixin.resource;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraftforge.resource.IResourceType;
+import net.minecraftforge.resource.VanillaResourceType;
+
+import net.minecraft.client.sound.SoundManager;
+
+import net.patchworkmc.impl.resource.TypedResourceLoader;
+
+@Mixin(SoundManager.class)
+public abstract class MixinSoundManager implements TypedResourceLoader {
+	@Override
+	public IResourceType getResourceType() {
+		return VanillaResourceType.SOUNDS;
+	}
+}
