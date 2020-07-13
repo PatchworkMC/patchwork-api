c9e0c1648c79cc9d5931157e669ef8e1bb1d6fa7
Kitlith <kitlith@kitl.pw>
2020-07-13 16:47:31 -0700

    Dispatch BasicEventHooks and ServerLifecycleHooks to LifecycleEvents

--- patchwork-god-classes/src/main/java/net/minecraftforge/fml/hooks/BasicEventHooks.java
+++ patchwork-god-classes/src/main/java/net/minecraftforge/fml/hooks/BasicEventHooks.java
@@ -19,9 +19,39 @@
 
 package net.minecraftforge.fml.hooks;
 
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+import net.patchworkmc.impl.event.lifecycle.LifecycleEvents;
+
 /*
  * Note: this class is intended for mod use only, to dispatch to the implementations kept in their own modules.
  * Do not keep implementation details here, methods should be thin wrappers around methods in other modules.
  */
 public class BasicEventHooks {
+	public static void onPlayerPreTick(PlayerEntity player) {
+		LifecycleEvents.firePlayerTickEvent(TickEvent.Phase.START, player);
+	}
+
+	public static void onPlayerPostTick(PlayerEntity player) {
+		LifecycleEvents.firePlayerTickEvent(TickEvent.Phase.END, player);
+	}
+
+	public static void onPreWorldTick(World world) {
+		LifecycleEvents.fireWorldTickEvent(TickEvent.Phase.START, world);
+	}
+
+	public static void onPostWorldTick(World world) {
+		LifecycleEvents.fireWorldTickEvent(TickEvent.Phase.END, world);
+	}
+
+	public static void onPreClientTick() {
+		LifecycleEvents.fireClientTickEvent(TickEvent.Phase.START);
+	}
+
+	public static void onPostClientTick() {
+		LifecycleEvents.fireClientTickEvent(TickEvent.Phase.END);
+	}
 }
