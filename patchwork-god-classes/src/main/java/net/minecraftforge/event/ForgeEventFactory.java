5bfeb7608d1802602ac2841d0c826fc88e2db16a
Kitlith <kitlith@kitl.pw>
2020-07-14 22:35:03 -0700

    Dispatch ForgeEventFactory.gatherCapabilities to CapabilityEvents

--- patchwork-god-classes/src/main/java/net/minecraftforge/event/ForgeEventFactory.java
+++ patchwork-god-classes/src/main/java/net/minecraftforge/event/ForgeEventFactory.java
@@ -19,9 +19,27 @@
 
 package net.minecraftforge.event;
 
+import javax.annotation.Nullable;
+
+import net.minecraftforge.common.capabilities.CapabilityDispatcher;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+
+import net.patchworkmc.impl.capability.CapabilityEvents;
+
 /*
  * Note: this class is intended for mod use only, to dispatch to the implementations kept in their own modules.
  * Do not keep implementation details here, methods should be thin wrappers around methods in other modules.
  */
 public class ForgeEventFactory {
+	// TODO: these keeps the forge restriction of T extends ICapabilityProvider, which patchwork otherwise does not enforce
+	// should it be removed?
+	@Nullable
+	public static <T extends ICapabilityProvider> CapabilityDispatcher gatherCapabilities(Class<? extends T> type, T provider) {
+		return gatherCapabilities(type, provider, null);
+	}
+
+	@Nullable
+	public static <T extends ICapabilityProvider> CapabilityDispatcher gatherCapabilities(Class<? extends T> type, T provider, @Nullable ICapabilityProvider parent) {
+		return CapabilityEvents.gatherCapabilities(type, provider, parent);
+	}
 }
