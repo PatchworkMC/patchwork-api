aafd620d700c5c0a6dead3f9f75bec941fad84e6
Kitlith <kitlith@kitl.pw>
2020-07-17 15:03:21 -0700

    fixup! checkstyle.

--- patchwork-events-world/src/main/java/net/patchworkmc/mixin/event/world/MixinThreadedAnvilChunkStorage.java
+++ patchwork-events-world/src/main/java/net/patchworkmc/mixin/event/world/MixinThreadedAnvilChunkStorage.java
@@ -19,8 +19,6 @@
 
 package net.patchworkmc.mixin.event.world;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.ChunkWatchEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
