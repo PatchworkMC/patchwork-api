fc2b42e0c96a6c2969726c7b5dce2b3dffe5c890
famous1622 <8428080+famous1622@users.noreply.github.com>
2020-07-18 23:21:51 -0400

    Remove useless import

--- patchwork-events-entity/src/main/java/net/patchworkmc/mixin/event/entity/MixinItemStack.java
+++ patchwork-events-entity/src/main/java/net/patchworkmc/mixin/event/entity/MixinItemStack.java
@@ -25,7 +25,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
