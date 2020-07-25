0e22a5bc0033f5b14d6aa695b8a144faa399022c
famous1622 <8428080+famous1622@users.noreply.github.com>
2020-07-24 23:27:01 -0400

    Use raw types to "hand erase" the generics (#151)

--- patchwork-registries/src/main/java/net/patchworkmc/mixin/registries/MixinFeature.java
+++ patchwork-registries/src/main/java/net/patchworkmc/mixin/registries/MixinFeature.java
@@ -19,6 +19,7 @@
 
 package net.patchworkmc.mixin.registries;
 
+import net.minecraftforge.registries.IForgeRegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
@@ -30,15 +31,15 @@ import net.patchworkmc.impl.registries.ExtendedForgeRegistryEntry;
 import net.patchworkmc.impl.registries.Identifiers;
 
 @Mixin(Feature.class)
-public class MixinFeature implements ExtendedForgeRegistryEntry<Feature> {
+public class MixinFeature implements ExtendedForgeRegistryEntry {
 	@Unique
 	private Identifier registryName;
 
 	@Override
-	public Feature setRegistryName(Identifier name) {
+	public IForgeRegistryEntry setRegistryName(Identifier name) {
 		this.registryName = name;
 
-		return (Feature) (Object) this;
+		return this;
 	}
 
 	public Identifier getRegistryName() {
--- patchwork-registries/src/main/java/net/patchworkmc/mixin/registries/MixinPotion.java
+++ patchwork-registries/src/main/java/net/patchworkmc/mixin/registries/MixinPotion.java
@@ -19,6 +19,7 @@
 
 package net.patchworkmc.mixin.registries;
 
+import net.minecraftforge.registries.IForgeRegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
@@ -30,15 +31,15 @@ import net.patchworkmc.impl.registries.ExtendedForgeRegistryEntry;
 import net.patchworkmc.impl.registries.Identifiers;
 
 @Mixin(Potion.class)
-public class MixinPotion implements ExtendedForgeRegistryEntry<Potion> {
+public class MixinPotion implements ExtendedForgeRegistryEntry {
 	@Unique
 	private Identifier registryName;
 
 	@Override
-	public Potion setRegistryName(Identifier name) {
+	public IForgeRegistryEntry setRegistryName(Identifier name) {
 		this.registryName = name;
 
-		return (Potion) (Object) this;
+		return this;
 	}
 
 	public Identifier getRegistryName() {
