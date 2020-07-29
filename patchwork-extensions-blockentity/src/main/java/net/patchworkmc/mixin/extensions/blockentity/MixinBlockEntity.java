2b0118bbd7382f5e5e228294fec060ef13627169
Rikka0w0 <929513338@qq.com>
2020-07-29 22:59:10 +1000

    Impl IForgeTileEntity.getTileData()

--- patchwork-extensions-blockentity/src/main/java/net/patchworkmc/mixin/extensions/blockentity/MixinBlockEntity.java
+++ patchwork-extensions-blockentity/src/main/java/net/patchworkmc/mixin/extensions/blockentity/MixinBlockEntity.java
@@ -20,10 +20,46 @@
 package net.patchworkmc.mixin.extensions.blockentity;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraftforge.common.extensions.IForgeTileEntity;
 
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.CompoundTag;
 
 @Mixin(BlockEntity.class)
 public abstract class MixinBlockEntity implements IForgeTileEntity {
+	@Unique
+	private CompoundTag customTileData;
+
+	@Inject(method = "fromTag", at = @At("RETURN"))
+	private void readForgeData(CompoundTag tag, CallbackInfo ci) {
+		if (tag.contains("ForgeData")) {
+			customTileData = tag.getCompound("ForgeData");
+		}
+	}
+
+	@Inject(method = "writeIdentifyingData", at = @At("RETURN"))
+	private void saveForgeData(CompoundTag tag, CallbackInfoReturnable<CompoundTag> ci) {
+		if (customTileData != null) {
+			tag.put("ForgeData", customTileData);
+		}
+	}
+
+	@Inject(method = "markRemoved", at = @At("RETURN"))
+	private void markRemoved(CallbackInfo ci) {
+		requestModelDataUpdate();
+	}
+
+	@Override
+	public CompoundTag getTileData() {
+		if (customTileData == null) {
+			customTileData = new CompoundTag();
+		}
+
+		return customTileData;
+	}
 }
