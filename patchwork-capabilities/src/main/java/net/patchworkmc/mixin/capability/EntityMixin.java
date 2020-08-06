ac6c1fb15410a619e4c4c73b1bd22cd3cc42c3c2
[object Object] <object-Object@outlook.com>
2020-08-06 11:12:25 -0700

    Fixed style & license

--- patchwork-capabilities/src/main/java/net/patchworkmc/mixin/capability/EntityMixin.java
+++ patchwork-capabilities/src/main/java/net/patchworkmc/mixin/capability/EntityMixin.java
@@ -26,7 +26,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
