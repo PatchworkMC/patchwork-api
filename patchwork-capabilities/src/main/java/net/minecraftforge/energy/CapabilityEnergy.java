04cbc4c8544b6627dde0b2903e8ff505bf6e457e
William Bradford Larcombe <william@williambl.com>
2020-08-20 22:32:43 +0000

    Add forge energy (#180)
    * Add forge energy
    Pretty much exact from YarnForge, with formatting changes
    
    * Fix license
    
    * Update fabric.mod.json
    
    * Move energy to patchwork-capabilities
    
    * Remove patchwork-energy

---
+++ patchwork-capabilities/src/main/java/net/minecraftforge/energy/CapabilityEnergy.java
@@ -0,0 +1,55 @@
+/*
+ * Minecraft Forge, Patchwork Project
+ * Copyright (c) 2016-2020, 2019-2020
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.energy;
+
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.Capability.IStorage;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+
+import net.minecraft.nbt.IntTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.util.math.Direction;
+
+public class CapabilityEnergy {
+	@CapabilityInject(IEnergyStorage.class)
+	public static Capability<IEnergyStorage> ENERGY = null;
+
+	public static void register() {
+		CapabilityManager.INSTANCE.register(IEnergyStorage.class, new IStorage<IEnergyStorage>() {
+					@Override
+					public Tag writeNBT(Capability<IEnergyStorage> capability, IEnergyStorage instance, Direction side) {
+						return new IntTag(instance.getEnergyStored());
+					}
+
+					@Override
+					public void readNBT(Capability<IEnergyStorage> capability, IEnergyStorage instance, Direction side, Tag nbt) {
+						if (!(instance instanceof EnergyStorage)) {
+							throw new IllegalArgumentException("Can not deserialize to an instance that isn't the default implementation");
+						}
+
+						((EnergyStorage) instance).energy = ((IntTag) nbt).getInt();
+					}
+				},
+				() -> new EnergyStorage(1000)
+		);
+	}
+}
+
