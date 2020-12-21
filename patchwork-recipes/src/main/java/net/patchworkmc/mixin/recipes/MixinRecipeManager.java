11957ba7a8d86617fdfc4c7e162c797f8433a63f
stairman06 <36215135+stairman06@users.noreply.github.com>
2020-12-20 23:41:01 -0600

    Remove leftover

--- patchwork-recipes/src/main/java/net/patchworkmc/mixin/recipes/MixinRecipeManager.java
+++ patchwork-recipes/src/main/java/net/patchworkmc/mixin/recipes/MixinRecipeManager.java
@@ -20,23 +20,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RecipeManager.class)
 public class MixinRecipeManager {
-//	@ModifyVariable(method = "apply", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;"))
-//	private Map<Identifier, JsonObject> patchwork_filterRecipeMap(Map<Identifier, JsonObject> map) {
-//		return map.entrySet().stream().filter(item -> {
-//			if (CraftingHelper.processConditions(item.getValue(), "conditions")) {
-//				System.out.println("conditions not met");
-//			}
-//
-//			return false;
-//		}).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-////	}
-//	@Redirect(method = "apply", at = @At(value = "INVOKE",target = "Ljava/util/Map;entrySet()Ljava/util/Set;"))
-//	private Set patchwork_filterRecipeSet(Map<Identifier, JsonObject> map) {
-//		return map.entrySet().stream().filter(item -> {
-//			return false;
-//		});
-//	}
-
 	@Redirect(method = "apply", at = @At(value = "INVOKE", target = "Ljava/util/Set;iterator()Ljava/util/Iterator;"))
 	private Iterator patchwork_filterRecipeSet(Set<Map.Entry<Identifier, JsonObject>> set) {
 		return set.stream().filter(item -> CraftingHelper.processConditions(item.getValue(), "conditions")).iterator();
