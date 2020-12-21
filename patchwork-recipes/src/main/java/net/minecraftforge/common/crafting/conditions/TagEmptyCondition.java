86aca068562c8baf72b56ba9081c9ccbc848b81a
stairman06 <36215135+stairman06@users.noreply.github.com>
2020-12-21 10:57:12 -0600

    More conditions

---
+++ patchwork-recipes/src/main/java/net/minecraftforge/common/crafting/conditions/TagEmptyCondition.java
@@ -0,0 +1,80 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016-2019.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.minecraftforge.common.crafting.conditions;
+
+import com.google.gson.JsonObject;
+
+import net.minecraft.item.Item;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+
+public class TagEmptyCondition implements ICondition {
+	private static final Identifier NAME = new Identifier("forge", "tag_empty");
+	private final Identifier tag_name;
+
+	public TagEmptyCondition(String location) {
+		this(new Identifier(location));
+	}
+
+	public TagEmptyCondition(String namespace, String path) {
+		this(new Identifier(namespace, path));
+	}
+
+	public TagEmptyCondition(Identifier tag) {
+		this.tag_name = tag;
+	}
+
+	@Override
+	public Identifier getID() {
+		return NAME;
+	}
+
+	@Override
+	public boolean test() {
+		Tag<Item> tag = ItemTags.getContainer().get(tag_name);
+		return tag == null || tag.values().isEmpty();
+	}
+
+	@Override
+	public String toString() {
+		return "tag_empty(\"" + tag_name + "\")";
+	}
+
+	public static class Serializer implements IConditionSerializer<TagEmptyCondition> {
+		public static final Serializer INSTANCE = new Serializer();
+
+		@Override
+		public void write(JsonObject json, TagEmptyCondition value) {
+			json.addProperty("tag", value.tag_name.toString());
+		}
+
+		@Override
+		public TagEmptyCondition read(JsonObject json) {
+			return new TagEmptyCondition(new Identifier(JsonHelper.getString(json, "tag")));
+		}
+
+		@Override
+		public Identifier getID() {
+			return TagEmptyCondition.NAME;
+		}
+	}
+}
--- patchwork-recipes/src/main/java/net/patchworkmc/impl/recipes/PatchworkRecipes.java
+++ patchwork-recipes/src/main/java/net/patchworkmc/impl/recipes/PatchworkRecipes.java
@@ -2,13 +2,25 @@ package net.patchworkmc.impl.recipes;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.conditions.AndCondition;
+import net.minecraftforge.common.crafting.conditions.FalseCondition;
+import net.minecraftforge.common.crafting.conditions.ItemExistsCondition;
 import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
 import net.minecraftforge.common.crafting.conditions.NotCondition;
+import net.minecraftforge.common.crafting.conditions.OrCondition;
+import net.minecraftforge.common.crafting.conditions.TrueCondition;
+import net.minecraftforge.common.crafting.conditions.TagEmptyCondition;
 
 public class PatchworkRecipes implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		CraftingHelper.register(NotCondition.Serializer.INSTANCE);
+		CraftingHelper.register(AndCondition.Serializer.INSTANCE);
+		CraftingHelper.register(FalseCondition.Serializer.INSTANCE);
+		CraftingHelper.register(ItemExistsCondition.Serializer.INSTANCE);
 		CraftingHelper.register(ModLoadedCondition.Serializer.INSTANCE);
+		CraftingHelper.register(NotCondition.Serializer.INSTANCE);
+		CraftingHelper.register(OrCondition.Serializer.INSTANCE);
+		CraftingHelper.register(TrueCondition.Serializer.INSTANCE);
+		CraftingHelper.register(TagEmptyCondition.Serializer.INSTANCE);
 	}
 }
